c0be110b0fbc0fd4334850f2eac55bc7eaf0def6
AristoteKABELU <20kk090@esisalama.org>
2023-08-15 15:05:11 +0200

    optimisation 2

--- showIpRoute.py
+++ showIpRoute.py
@@ -1,11 +1,5 @@
-from init_router import R1, R2
+from init_router import *
 from netmiko import ConnectHandler
 
-netConnect = ConnectHandler(**R1)
-output_router1 = netConnect.send_command('show ip route')
-
-netConnect = ConnectHandler(**R2)
-output_router2 = netConnect.send_command('show ip route')
-print(output_router1,
-      '\n------------------------------------------------------------------------------\n',
-      output_router2)
+for route in routers:
+    print(ConnectHandler(**route).send_command('show ip route'))
