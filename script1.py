43a9e3c8c55e8b50a25573c552587430fe80349d
AristoteKABELU <20kk090@esisalama.org>
2023-08-16 09:03:59 +0200

    optimisation v3

--- script1.py
+++ script1.py
@@ -3,12 +3,7 @@ from init_router import *
 
 for router in routers:
     net_connect = ConnectHandler(**router)
-    configurations = [
-        'router ospf 1',
-        'network 10.10.20.0 0.0.0.255 area 10',
-        'network 10.10.10.0 0.0.0.255 area 10',
-        'network 192.168.122.0 0.0.0.255 area 10'
-    ]
+    configurations = []
     for v in router.values():
         if v == 'scripting1':
             configurations.append(['interface f1/0',
@@ -18,5 +13,12 @@ for router in routers:
             configurations.append(['interface f1/0',
                                    'ip address 10.10.20.1 255.255.255.0',
                                    'no shutdown'])
+    configurations.append([
+        'router ospf 1',
+        'network 10.10.20.0 0.0.0.255 area 10',
+        'network 10.10.10.0 0.0.0.255 area 10',
+        'network 192.168.122.0 0.0.0.255 area 10'
+    ])
+
     for conf in configurations:
-        net_connect.send_config_set(conf)
+        print(net_connect.send_config_set(conf))
